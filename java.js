44fd231eea7cd41eff16b3588979d40f4e090503
muscab101 <muscabmusa27@gmail.com>
2025-10-24 18:04:10 +0100

    navbar responsive

---
+++ java.js
@@ -0,0 +1,21 @@
+// let navBar = document.querySelector('#nav')
+// let burger = document.querySelector('#burger')
+
+// navBar.addEventListener('click', ()=> {
+//         burger.classList.toggle('active');
+// })
+
+
+function Menu(e) {
+  const list = document.querySelector('#burger');
+
+  if (e.name === 'menu') {
+    e.name = 'close';
+    list.classList.add('top-[80px]');
+    list.classList.add('opacity-100');
+  } else {
+    e.name = 'menu';
+    list.classList.remove('top-[80px]');
+    list.classList.remove('opacity-100');
+  }
+}
